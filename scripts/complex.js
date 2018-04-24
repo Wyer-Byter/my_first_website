81ada47d1679b82e7888f0f27fba146814fd9749
Jon Stephenson <stephensonjl@innovasi.com>
2018-04-24 16:21:43 -0400

    Adding mandelbrott page.

--- scripts/complex.js
+++ scripts/complex.js
@@ -23,7 +23,7 @@ function multiplication (z, w) {
     return v;
 }
 
-function division(z, w) {
+function division (z, w) {
     var v = {};
     v.a = (z.a * w.a + z.b * w.b) / (w.a * w.a + w.b * w.b);
     v.b = (z.b * w.a - z.a * w.b) / (w.a * w.a + w.b * w.b);
